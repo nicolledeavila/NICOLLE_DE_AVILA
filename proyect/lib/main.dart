f53e2f22dcd23e5cab865e8d2df6de62f58f6ea4
nicolledeavila <168228839+nicolledeavila@users.noreply.github.com>
2025-08-29 05:56:08 -0500

    y

--- proyect/lib/main.dart
+++ proyect/lib/main.dart
@@ -1,93 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:proyect/features/home/views/errorViews.dart';
+import 'package:proyect/features/home/views/initialViews.dart';
+import 'package:proyect/features/home/views/loadingViews.dart';
+import 'package:proyect/features/home/views/successViews.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('Trabajo')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 32),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(24),
-              margin: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: const Color.fromARGB(255, 238, 31, 31),
-              ),
-              child: Text(
-                'Hola, carlos\nContacto: carlos@example.com\nSaldo: 100',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: EdgeInsets.only(left: 20, bottom: 8),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text('Tareas', style: TextStyle(fontSize: 24))],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.zero,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.transparent,
-              ),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 100,
-                      child: Card(
-                        color: Colors.green,
-                        margin: EdgeInsets.zero,
-                        child: Center(
-                          child: Text(
-                            'Comprar\nir al ara',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 100,
-                      child: Card(
-                        color: Colors.blue,
-                        margin: EdgeInsets.zero,
-                        child: Center(
-                          child: Text(
-                            'Transito\ntramites',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: const ErrorView(),
     );
   }
 }
